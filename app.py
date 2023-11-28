dadcae463440e6c3678f3f7325ceaa9bf39769e2
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-11-28 23:10:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -19,17 +20,17 @@ def load_stock_data(file_path):
 # Function to calculate correlation and trendline analysis
 def calculate_correlation_and_trend(stock_data, industry_data, selected_stock, selected_industry):
     # Filter selected stock and industry data
-    selected_stock_data = stock_data.loc[selected_stock]
+    selected_stock_data = stock_data
     selected_industry_data = industry_data[selected_industry]
 
     # Merge stock and industry data on the "Date" column
     merged_data = pd.merge(selected_stock_data, selected_industry_data, left_index=True, right_index=True)
 
     # Calculate correlation between stock revenue and industry growth
-    correlation = merged_data["Total Revenue/Income"].corr(merged_data[selected_industry])
+    correlation = merged_data["Close"].corr(merged_data[selected_industry])
 
     # Prepare data for trendline analysis
-    X = merged_data["Total Revenue/Income"].values.reshape(-1, 1)
+    X = merged_data["Close"].values.reshape(-1, 1)
     y = merged_data[selected_industry].values.reshape(-1, 1)
 
     # Fit a linear regression model
@@ -43,35 +44,35 @@ def calculate_correlation_and_trend(stock_data, industry_data, selected_stock, s
 def main():
     st.title("Stock and Industry Analysis App")
 
-    # Upload stock revenue file
-    uploaded_file = st.file_uploader("Upload Stock Revenue File (in xlsx format)", type="xlsx")
+    # Get list of stock files in the "Stock_Data" folder
+    stock_folder_path = "Stock_Data"
+    stock_files = [file for file in os.listdir(stock_folder_path) if file.endswith(".xlsx")]
 
-    stock_data = None
-    if uploaded_file is not None:
-        stock_data = load_stock_data(uploaded_file)
+    # Select stock file
+    selected_stock_file = st.selectbox("Select Stock File", stock_files)
+
+    # Load selected stock data
+    stock_data = load_stock_data(os.path.join(stock_folder_path, selected_stock_file))
 
     if stock_data is None:
         # If there's an issue loading stock data, exit the function
         return
 
-    # Select industry and stock
+    # Select industry
     selected_industry = st.selectbox("Select Industry", industry_data.columns[1:])
-    
-    # Modify this line based on the actual stock symbols in your data
-    selected_stock = st.selectbox("Select Stock", stock_data.index.unique())
 
     # Calculate correlation and trendline
-    correlation, trendline = calculate_correlation_and_trend(stock_data, industry_data, selected_stock, selected_industry)
+    correlation, trendline = calculate_correlation_and_trend(stock_data, industry_data, selected_industry, selected_industry)
 
     # Plotting
     plt.figure(figsize=(10, 6))
 
     # Plot stock revenue and industry growth
-    plt.scatter(stock_data.loc[selected_stock]["Total Revenue/Income"], industry_data[selected_industry], label="Data points")
-    plt.plot(stock_data.loc[selected_stock]["Total Revenue/Income"], trendline, color='red', label="Trendline")
+    plt.scatter(stock_data["Close"], industry_data[selected_industry], label="Data points")
+    plt.plot(stock_data["Close"], trendline, color='red', label="Trendline")
 
     plt.title(f"Correlation: {correlation:.2f} | Trendline Analysis")
-    plt.xlabel("Total Revenue/Income")
+    plt.xlabel("Stock Close Price")
     plt.ylabel(f"{selected_industry} (Industry Growth)")
     plt.legend()
     st.pyplot()
